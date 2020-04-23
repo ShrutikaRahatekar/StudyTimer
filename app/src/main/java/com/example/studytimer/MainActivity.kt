557cac7b44dd38d27d1fd346cb22c3d7d63bb6ec
Shrutika Rahatekar <shrutikarahatekar43@gmail.com>
2020-04-23 17:16:13 +0530

    done

--- app/src/main/java/com/example/studytimer/MainActivity.kt
+++ app/src/main/java/com/example/studytimer/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.studytimer
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.os.SystemClock
 import android.util.Log
 import android.widget.Chronometer
@@ -11,73 +10,82 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-
 class MainActivity : AppCompatActivity() {
-    var meter:Chronometer? = null
     var preference: SharedPreferences? = null
     var isWorking = false
-    public var pauseOffset: Long=0
-    var st:String=""
+    var pauseOffset: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        preference = getPreferences(Context.MODE_PRIVATE)
 
-       preference = getPreferences(Context.MODE_PRIVATE)
-        meter = findViewById(R.id.c_meter)
+        textView.text = "you spend ${getData()} time for studying last time."
+
 
         start.setOnClickListener {
             if (!isWorking) {
-                meter?.base = SystemClock.elapsedRealtime() - pauseOffset
-                meter?.start()
+                c_meter.base = SystemClock.elapsedRealtime() - pauseOffset
+                c_meter.start()
                 isWorking = true
             }
-            Toast.makeText(this,"start",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "start", Toast.LENGTH_LONG).show()
         }
         pause.setOnClickListener {
             if (isWorking) {
-                meter?.stop()
-                pauseOffset = SystemClock.elapsedRealtime() - (meter?.base ?: 0)
+                c_meter.stop()
+                pauseOffset = SystemClock.elapsedRealtime() - c_meter.base
+                Log.d("HELLO: pause", pauseOffset.toString())
+                Log.d("HELLO: base", c_meter.base.toString())
+                Log.d("HELLO", SystemClock.elapsedRealtime().toString())
                 isWorking = false
             }
-            Toast.makeText(this,"pause",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "pause", Toast.LENGTH_LONG).show()
 
         }
         save.setOnClickListener {
+            if (isWorking) {
+                c_meter.stop()
+                pauseOffset = SystemClock.elapsedRealtime() - (c_meter.base ?: 0)
+                isWorking = false
+            }
             saveData()
-            textView.text = "you spend ${getData()} time for studing lasttime"
-            meter?.base = SystemClock.elapsedRealtime();
+            textView.text = "you spend ${getData()} time for studying last time."
             pauseOffset = 0;
-            Toast.makeText(this,"save",Toast.LENGTH_LONG).show()
+            c_meter.base = SystemClock.elapsedRealtime();
+            Toast.makeText(this, "save", Toast.LENGTH_LONG).show()
         }
 
     }
-    private fun saveData(){
-        val editor=preference?.edit() ?: return
-        editor.putString("last_read",meter?.text.toString())
+
+    private fun saveData() {
+        if (isWorking) {
+            c_meter.stop()
+            pauseOffset = SystemClock.elapsedRealtime() - (c_meter.base ?: 0)
+            isWorking = false
+        }
+        val editor = preference?.edit() ?: return
+        editor.putString("last_read", c_meter.text.toString())
         editor.apply()
     }
 
-    private fun getData(): String{
+    private fun getData(): String {
         return preference?.getString("last_read", "00:00") ?: "00:00"
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         outState.run {
-            pauseOffset = SystemClock.elapsedRealtime() - (meter?.base ?: 0)
-
-            putLong("last_time", pauseOffset)
+            putLong("last_time", SystemClock.elapsedRealtime() - c_meter.base)
             putBoolean("isWorking", isWorking)
         }
         super.onSaveInstanceState(outState)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        pauseOffset = savedInstanceState.getLong("last_time")
-        meter?.base = SystemClock.elapsedRealtime() - pauseOffset
         isWorking = savedInstanceState.getBoolean("isWorking")
-        if(isWorking){
-            meter?.start()
+        c_meter.base = SystemClock.elapsedRealtime() - savedInstanceState.getLong("last_time")
+        if (isWorking) {
+            c_meter.start()
         }
         super.onRestoreInstanceState(savedInstanceState)
     }
